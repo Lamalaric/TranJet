fb8ea1a7296661aba3d5c9c5937a1e0183203179
Amalaric <lamalaric93@gmail.com>
2021-10-29 16:39:16 +0200

    Big Black Cock commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Amalaric Le Forestier</title>
+	<meta name="author" content="La communauté communiste" />
+	<meta name="description" content="Tran le dictateur">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8" />
+	<script src="https://kit.fontawesome.com/79f61c2135.js" crossorigin="anonymous"></script>
+<!--	<link rel="icon" href="images/logo.ico" />-->
+	<link rel="stylesheet" href="styles/style.css" type="text/css" />
+	<link rel="stylesheet" href="styles/mediaqueries.css" type="text/css" />
+	<script src="scripts/script.js" ></script>
+</head>
+
+
+
+<body>
+
+	<header>
+		<nav>
+		</nav>
+	</header>
+
+	<main>
+    </main>
+
+    <footer>
+    </footer>
+
+</body>
+</html>
