93020b7c7d27205b771ad337df50b9e6e585db4e
Amalaric <lamalaric93@gmail.com>
2021-11-07 15:15:52 +0100

    Files structure amended

--- index.php
+++ index.php
@@ -1,30 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <title>Amalaric Le Forestier</title>
-    <meta name="author" content="La communauté communiste" />
-    <meta name="description" content="Tran le dictateur">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8" />
-    <script src="https://kit.fontawesome.com/79f61c2135.js" crossorigin="anonymous"></script>
-    <!--	<link rel="icon" href="images/logo.ico" />-->
-    <link rel="stylesheet" href="styles/style.css" type="text/css" />
-    <link rel="stylesheet" href="styles/mediaqueries.css" type="text/css" />
-    <script src="scripts/script.js" ></script>
-</head>
-
-<body>
-
 <?php
 include("includes/header.inc.php");
 ?>
 
+
+<body>
 <main>
 </main>
+</body>
+
 
 <?php
 include("includes/footer.inc.php");
 ?>
-
-</body>
-</html>
