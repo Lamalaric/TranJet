ed055b931309579a8615bdd72b8889111e2b1e92
Amalaric <lamalaric93@gmail.com>
2021-11-21 16:47:19 +0100

    Style for the creation page

--- front/creation.php
+++ front/creation.php
@@ -10,18 +10,26 @@ include("includes/header.inc.php");
             <img src="images/creation1.png" alt="Bonhomme pierre feuille ciseau">
         </aside>
 
-        <!--  BLOC CONNEXION  -->
+        <!--  BLOC CREATION DE COMPTE  -->
         <section>
-            <h2>Créer un compte</h2>
             <form action="" method="POST">
                 <input type="text" name="nom" placeholder="Nom" required>
                 <input type="text" name="prenom" placeholder="Prénom" required>
                 <input type="text" name="adresse" placeholder="Adresse" required>
                 <input type="text" name="mail" placeholder="Adresse mail" required>
                 <input type="text" name="raison_sociale" placeholder="Raison sociale" required>
-                <input type="password" name="mdp" placeholder="Mot de passe" required>
-                <input type="password" name="mdp_confirm" placeholder="Confirmer le mot de passe" required>
+                <div class="container-pwd">
+                    <span>
+                        <input class="input-pwd" id="pwd1" type="password" name="mdp" placeholder="Mot de passe" required>
+                        <i onclick="togglePasswordVisibility('pwd1')" class="fas fa-low-vision"></i>
+                    </span>
+                    <span>
+                        <input class="input-pwd" id="pwd2" type="password" name="mdp_confirm" placeholder="Confirmer mdp" required>
+                        <i onclick="togglePasswordVisibility('pwd2')" class="fas fa-low-vision"></i>
+                    </span>
+                </div>
                 <input type="submit" name="btn-creation" value="Créer le compte">
+
             </form>
         </section>
 
